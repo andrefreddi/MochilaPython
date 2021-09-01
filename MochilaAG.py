7871d94ae0d70f4937fee87f05ff0b2547d9be48
andre.freddi <52337680+andrefreddi@users.noreply.github.com>
2021-08-31 21:08:30 -0300

    Add files via upload

---
+++ MochilaAG.py
@@ -0,0 +1,33 @@
+
+from genetic2020 import *
+                  #[peso,valor]
+pesos_e_valores = [[10, 90], [8, 10], [8, 30], [25, 75], \
+                   [2, 10], [50, 100], [6, 300], [12, 50], \
+                   [100, 400], [8, 300]]
+peso_maximo = 100 #
+n_de_cromossomos = 220 #Numero de Cromossomos
+geracoes = 150 #Numero de geracoes
+n_de_itens = len(pesos_e_valores) #Analogo aos pesos e valores
+
+
+
+#EXECUCAO DOS PROCEDIMENTOS
+populacao = population(n_de_cromossomos, n_de_itens)
+historico_de_fitness = [media_fitness(populacao, peso_maximo, pesos_e_valores)]
+for i in range(geracoes):
+    populacao = evolve(populacao, peso_maximo, pesos_e_valores, n_de_cromossomos)
+    historico_de_fitness.append(media_fitness(populacao, peso_maximo, pesos_e_valores))
+
+
+
+#PRINTS DO TERMINAL
+for indice,dados in enumerate(historico_de_fitness):
+   print ("Geracao: ", indice," | Media de valor na mochila: ", dados)
+
+print("\nPeso máximo:",peso_maximo,"g\n\nItens disponíveis:")
+for indice,i in enumerate(pesos_e_valores):
+    print("Item ",indice+1,": ",i[0],"kg | Valor:",i[1])
+    
+print("\nExemplos de boas solucoes: ")
+for i in range(5):
+    print(populacao[i])
